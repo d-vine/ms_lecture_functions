3046665b044a9dae3185d1058275a5842ba26425
Georg Goettlich <mail@dvine.de>
2024-03-09 14:26:47 -0500

    created circle function

--- lesson.js
+++ lesson.js
@@ -1,20 +1,24 @@
+function circle(x,y,diameter) {
+    ellipse(x, y, diameter, diameter);
+}
+
 function draw() {
     background(255, 255, 255);
-    
+
     //smiley
     fill(230, 230, 0);
-    ellipse(100, 100, 100, 100);
+    circle(100, 100, 100);
     fill(255, 255, 255);
-    ellipse(80, 90, 20, 20);
-    ellipse(120, 90, 20, 20);
+    circle(80, 90, 20);
+    circle(120, 90, 20);
     fill(0,0,0);
-    ellipse(80,90,5,5);
-    ellipse(120,90,5,5);
+    circle(80, 90, 5);
+    circle(120, 90, 5);
     noFill();
     arc(100, 110, 60, 40, 0, 180);
 
     //stick figure
-    ellipse(300,100,30,30);
+    circle(300, 100, 30);
     line(300,115,300,155);
     line(300,155,275,190);
     line(300,155,325,190);
