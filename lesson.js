c02bc49ecf12c3b08ab0b9f034bb9e137716e5ce
Georg Goettlich <mail@dvine.de>
2024-03-09 17:17:50 -0500

    added color parameter

--- lesson.js
+++ lesson.js
@@ -9,8 +9,8 @@ function smiley_eye(x,y, size) {
     circle(x, y, size * 0.05);
 }
 
-function smiley(x, y, size) {
-    fill(230, 230, 0);
+function smiley(x, y, size, bg_color) {
+    fill(bg_color);
     circle(x, y, size);
     smiley_eye(x - size * 0.2, y - size * 0.1, size);
     smiley_eye(x + size * 0.2, y - size * 0.1, size);
@@ -19,8 +19,8 @@ function smiley(x, y, size) {
     arc(x, y + size * 0.1, size * 0.6, size * 0.4, 0, 180);
 }
 
-function stick_figure(x, y, size) {
-    smiley(x, y - size * 0.5, size * 0.3);
+function stick_figure(x, y, size, face_color) {
+    smiley(x, y - size * 0.5, size * 0.3, face_color);
     line(x, y - size * 0.35, x , y + size * 0.05);
     line(x, y + size * 0.05, x - size * 0.25, y + size * 0.4);
     line(x, y + size * 0.05, x + size * 0.25, y + size * 0.4);
@@ -28,14 +28,16 @@ function stick_figure(x, y, size) {
     line(x, y - size * 0.2, x - size * 0.25, y - size * 0.4);
 }
 
+var smiley_color = color(230, 230, 0);
+
 function draw() {
     background(255, 255, 255);
-    smiley(50, 50, 100);
-    smiley(150, 150, 90);
-    smiley(250, 250, 80);
-    smiley(350, 350, 70);
-    stick_figure(233, 73, 100);
-    stick_figure(353, 183, 90);
-    stick_figure(58, 202, 80);
-    stick_figure(177, 348, 70);
+    smiley(50, 50, 100, smiley_color);
+    smiley(150, 150, 90, smiley_color);
+    smiley(250, 250, 80, smiley_color);
+    smiley(350, 350, 70, smiley_color);
+    stick_figure(233, 73, 100, smiley_color);
+    stick_figure(353, 183, 90, smiley_color);
+    stick_figure(58, 202, 80, smiley_color);
+    stick_figure(177, 348, 70, smiley_color);
 }
