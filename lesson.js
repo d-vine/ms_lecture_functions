0bf32d6d3cc0ab2c245edabe05f8bc077a30ea9d
Georg Goettlich <mail@dvine.de>
2024-03-09 14:33:25 -0500

    added smiley_eye function

--- lesson.js
+++ lesson.js
@@ -2,18 +2,22 @@ function circle(x,y,diameter) {
     ellipse(x, y, diameter, diameter);
 }
 
+function smiley_eye(x,y) {
+    fill(255, 255, 255);
+    circle(x, y, 20);
+    fill(0,0,0);
+    circle(x, y, 5);
+}
+
 function draw() {
     background(255, 255, 255);
-
+    
     //smiley
     fill(230, 230, 0);
     circle(100, 100, 100);
-    fill(255, 255, 255);
-    circle(80, 90, 20);
-    circle(120, 90, 20);
-    fill(0,0,0);
-    circle(80, 90, 5);
-    circle(120, 90, 5);
+    smiley_eye(80,90);
+    smiley_eye(120,90);
+    smiley_eye(100,70);
     noFill();
     arc(100, 110, 60, 40, 0, 180);
 
