caed4afa88e0ddd26ed660544eb29b79de605952
Georg Goettlich <mail@dvine.de>
2024-03-09 16:50:20 -0500

    added scaling

--- lesson.js
+++ lesson.js
@@ -2,40 +2,40 @@ function circle(x,y,diameter) {
     ellipse(x, y, diameter, diameter);
 }
 
-function smiley_eye(x,y) {
+function smiley_eye(x,y, size) {
     fill(255, 255, 255);
-    circle(x, y, 20);
+    circle(x, y, size * 0.2);
     fill(0,0,0);
-    circle(x, y, 5);
+    circle(x, y, size * 0.05);
 }
 
-function smiley(x, y) {
+function smiley(x, y, size) {
     fill(230, 230, 0);
-    circle(x, y, 100);
-    smiley_eye(x - 20, y - 10);
-    smiley_eye(x + 20, y - 10);
-    smiley_eye(x, y - 30);
+    circle(x, y, size);
+    smiley_eye(x - size * 0.2, y - size * 0.1, size);
+    smiley_eye(x + size * 0.2, y - size * 0.1, size);
+    smiley_eye(x, y - size * 0.3, size);
     noFill();
-    arc(x, y + 10, 60, 40, 0, 180);
+    arc(x, y + size * 0.1, size * 0.6, size * 0.4, 0, 180);
 }
 
-function stick_figure(x, y) {
-    circle(x, y - 50, 30);
-    line(x, y - 35, x , y + 5);
-    line(x, y + 5, x - 25, y + 40);
-    line(x, y + 5, x + 25, y + 40);
-    line(x, y - 20, x + 25, y - 40);
-    line(x, y - 20, x - 25, y - 40);
+function stick_figure(x, y, size) {
+    circle(x, y - size * 0.5, size * 0.3);
+    line(x, y - size * 0.35, x , y + size * 0.05);
+    line(x, y + size * 0.05, x - size * 0.25, y + size * 0.4);
+    line(x, y + size * 0.05, x + size * 0.25, y + size * 0.4);
+    line(x, y - size * 0.2, x + size * 0.25, y - size * 0.4);
+    line(x, y - size * 0.2, x - size * 0.25, y - size * 0.4);
 }
 
 function draw() {
     background(255, 255, 255);
-    smiley(50, 50);
-    smiley(150, 150);
-    smiley(250, 250);
-    smiley(350, 350);
-    stick_figure(233, 73);
-    stick_figure(353, 183);
-    stick_figure(58, 202);
-    stick_figure(177, 348);
+    smiley(50, 50, 100);
+    smiley(150, 150, 90);
+    smiley(250, 250, 80);
+    smiley(350, 350, 70);
+    stick_figure(233, 73, 100);
+    stick_figure(353, 183, 90);
+    stick_figure(58, 202, 80);
+    stick_figure(177, 348, 70);
 }
